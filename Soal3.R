d7ac59533df1f2d3e53e89b019f2d1c7b4c7cc64
meisyalsabila7 <94627623+meisyalsabila7@users.noreply.github.com>
2022-04-10 15:09:15 +0700

    Done

---
+++ Soal3.R
@@ -0,0 +1,16 @@
+lamda = 4.5
+
+### 3.A
+x = 6
+Peluang = dpois(x, lamda)
+
+## 3.B
+x = 6
+n = 356
+hist(rpois(n, lamda), main = "Histogram Poisson")
+
+### 3.C
+#Perbandingan tidak terpaut jauh
+
+### 3.D
+#Mean dan varian ialah lamda
